f55f8825fa1559a8a31cca917d436dc12701a598
Jools Torrente Calo <97971137+joolstorrentecalo@users.noreply.github.com>
2024-02-27 21:40:45 +0100

    Initialize flutter-2.3-candidate.27

---
+++ packages/flutter_tools/lib/src/globals.dart
@@ -0,0 +1,26 @@
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// @dart = 2.8
+
+import 'base/context.dart';
+import 'device.dart';
+import 'doctor.dart';
+import 'fuchsia/fuchsia_sdk.dart';
+import 'ios/simulators.dart';
+import 'macos/xcdevice.dart';
+import 'reporting/crash_reporting.dart';
+import 'runner/local_engine.dart';
+
+export 'globals_null_migrated.dart';
+
+CrashReporter get crashReporter => context.get<CrashReporter>();
+Doctor get doctor => context.get<Doctor>();
+DeviceManager get deviceManager => context.get<DeviceManager>();
+
+LocalEngineLocator get localEngineLocator => context.get<LocalEngineLocator>();
+FuchsiaArtifacts get fuchsiaArtifacts => context.get<FuchsiaArtifacts>();
+IOSSimulatorUtils get iosSimulatorUtils => context.get<IOSSimulatorUtils>();
+
+XCDevice get xcdevice => context.get<XCDevice>();
