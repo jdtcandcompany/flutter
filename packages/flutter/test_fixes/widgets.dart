3abd96561ad0ff6b6ceaf3049ab01566d8d1de88
Jools Torrente Calo <97971137+joolstorrentecalo@users.noreply.github.com>
2024-02-27 21:35:53 +0100

    Initialize flutter-1.26-candidate.10

---
+++ packages/flutter/test_fixes/widgets.dart
@@ -0,0 +1,16 @@
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/widgets.dart';
+
+void main() {
+  // Changes made in https://github.com/flutter/flutter/pull/45941
+  final WidgetsBinding binding = WidgetsBinding.instance!;
+  binding.deferFirstFrameReport();
+  binding.allowFirstFrameReport();
+
+  // Changes made in https://github.com/flutter/flutter/pull/44189
+  const StatefulElement statefulElement = StatefulElement(myWidget);
+  statefulElement.inheritFromElement(ancestor);
+}
